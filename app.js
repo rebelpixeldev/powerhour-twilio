38abaa517be74e04c6afdb36ade13633e23d9117
Ted Sczelecki <tsczelecki@konradgroup.com>
2016-09-15 00:42:11 -0400

    Testing recieving text messages

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
+const config = require('./config');
+
 const
     _       = require('lodash'),
+    client  = require('twilio')(config.accountSid, config.authToken)
     express = require('express'),
     path    = require('path'),
     swig    = require('swig'),
@@ -12,6 +15,10 @@ app.engine('html', swig.renderFile);
 app.set("view engine", "html");
 app.set('views', path.join(__dirname, '/app/views/'));
 
+
+/** * * * * * * * * * * * * * * *
+ * Voice
+ */
 for ( let i = 0; i <= 5; i++ ){
     app.get('/voice/twiml/' + i, (req, res) => {
         res.header('Content-Type', 'application/xml');
@@ -27,6 +34,18 @@ app.get('/voice/gather', (req, res) => {
     res.header('Content-Type', 'application/xml');
     res.render('twiml/gather', { params:params, digitsTalk : params.Digits.toString().split('').join(' ')} );
 });
+
+/** * * * * * * * * * * * * * * *
+ * Sms
+ */
+
+app.get('/sms/recieve', (req, res) => {
+    const params = url.parse(req.url, true).query;
+    console.log('Text message recieved * * * * * * * * * * * * * * * * ');
+    console.log(params);
+    numbers.push(params.Caller);
+    numbers = _.uniq(numbers);
+})
 
 app.listen(3000, () => {
     console.log('Connected on 3000');
---
+++ config.js
@@ -0,0 +1,5 @@
+
+module.exports = {
+  accountSid : "ACd5d40b7b2e6b2c3edb5cf1f01a3027ad",
+  authToken : "6617116da9da8d1b9810ffc70d53b7ce"
+}
