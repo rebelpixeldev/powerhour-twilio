ce02d6b884be639aa946d0ef762f92547762a3a8
Ted Sczelecki <tsczelecki@konradgroup.com>
2016-09-15 00:17:44 -0400

    Testing final twiml demo

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const
+    _       = require('lodash'),
     express = require('express'),
     path    = require('path'),
     swig    = require('swig'),
@@ -21,7 +22,7 @@ for ( let i = 0; i <= 5; i++ ){
 app.get('/voice/gather', (req, res) => {
     const params = url.parse(req.url, true).query;
     console.log(params);
-    numbers.push(params.Caller);
+    _.uniq(numbers.push(params.Caller));
     console.log(numbers);
     res.header('Content-Type', 'application/xml');
     res.render('twiml/gather', { params:params, digitsTalk : params.Digits.toString().split('').join(' ')} );
